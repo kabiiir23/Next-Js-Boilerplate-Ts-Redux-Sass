ac76151ba1aefec65fa535a34bc95f0c77b217c5
kabiiir23 <shourav93@gmail.com>
2021-07-12 19:55:07 +0600

    Added Babel Config

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-  presets: ['next/babel'],
-  overrides: [
-    {
-      include: ['./node_modules'],
-      plugins: [
-        [
-          '@iwatakeshi/babel-plugin-remove-import-styles',
-          {
-            extensions: ['.css'],
-          },
-        ],
-      ],
-    },
-    {
-      plugins:
-        process.env.NODE_ENV == 'production'
-          ? ['transform-remove-console']
-          : [],
-    },
-  ],
+	presets: ['next/babel'],
+	overrides: [
+		{
+			/** NextJS does not support css imports within modules. This plugin removes the css imports within node_modules. Afterwards you need to import the css file in `_app.tsx` file */
+			include: ['./node_modules'],
+			plugins: [
+				[
+					'@iwatakeshi/babel-plugin-remove-import-styles',
+					{
+						extensions: ['.css'],
+					},
+				],
+			],
+		},
+		{
+			/** This plugin removes all the console.log from production build */
+			plugins:
+				process.env.NODE_ENV == 'production'
+					? ['transform-remove-console']
+					: [],
+		},
+	],
 };
