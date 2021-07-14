5628eaae4d18e34d997e7b1dcfe9a9cbcec9eba4
Kabir <shourav93@gmail.com>
2021-07-14 11:12:33 +0600

    Added User Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,26 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import ButtonPrimary from '../components/ButtonPrimary';
+import User from '@components/Features/User Components/User';
+import ButtonPrimary from '../components/UI/ButtonPrimary';
 import AppLayout from '../layouts/AppLayout';
 import styles from './Home.module.scss';
 
 export default function Home() {
-  return (
-    <AppLayout pageTitle='Markopolo.ai' description='Markopolo.ai'>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <img src='/kabiiir23.svg' alt='kabiiir23' />
-          <ButtonPrimary onClick={() => {}}>
-            <a
-              href='https://github.com/kabiiir23'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Github
-            </a>
-          </ButtonPrimary>
-        </main>
-      </div>
-    </AppLayout>
-  );
+	return (
+		<AppLayout pageTitle='Markopolo.ai' description='Markopolo.ai'>
+			<div className={styles.container}>
+				<main className={styles.main}>
+					<img src='/kabiiir23.svg' alt='kabiiir23' width='80px' />
+					<ButtonPrimary onClick={() => {}}>
+						<a
+							href='https://github.com/kabiiir23'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							Github
+						</a>
+					</ButtonPrimary>
+					<User />
+				</main>
+			</div>
+		</AppLayout>
+	);
 }
